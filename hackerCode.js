01079077ddfa016935ded13b7875df31a16bf447
Sean Haile <seanhaile.codes@gmail.com>
2021-07-29 22:11:36 -0600

    code now runs but with unexpected response

--- hackerCode.js
+++ hackerCode.js
@@ -40,3 +40,32 @@ function Person(initialAge){
  yearPasses: yearPasses(){
         return ++person.age
        }
+
+
+      //  Updated Response
+
+
+      function Person(initialAge){
+            if (initialAge <0){
+            age = initialAge
+            }
+            else {
+                age = 0
+                console.log("Age is not valid, setting age to 0.")
+            }
+        
+      this.amIOld=function(){
+        if (age < 13){
+            console.log('You are young.')
+        }
+        else if(age > 13 && age < 18) {
+                console.log('You are a teenager.')
+        }
+        else {
+            console.log('You are old')
+        }
+      };
+        this.yearPasses=function(){
+            age++
+    };
+  }
