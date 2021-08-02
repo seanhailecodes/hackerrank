050c572dfee8d032d701f74437c2adafde7765f4
Sean Haile <seanhaile.codes@gmail.com>
2021-08-02 16:19:44 -0600

    lil loop action to start day 5

--- hackerCode.js
+++ hackerCode.js
@@ -28,3 +28,5 @@ this.amIOld = function(){
  };
 }
 
+
+// day 5 
---
+++ loops.js
@@ -0,0 +1,12 @@
+// Loops
+// not working yet
+
+var n = 3
+
+function multiply(n) {
+for (var n = 0; n <= 10; n = n*+) {
+  console.log(n)
+}
+}
+
+multiply(n)
