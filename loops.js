17a8ff07facb236226d43cb959776616784b85ce
Sean Haile <seanhaile.codes@gmail.com>
2021-08-03 18:45:28 -0600

    some running code not yet final

--- loops.js
+++ loops.js
@@ -10,3 +10,23 @@ for (var n = 0; n <= 10; n = n*+) {
 }
 
 multiply(n)
+
+// not working but closer 
+
+var n = 3
+
+function print(n){
+
+  while ( n < 11){
+      console.log(n)
+    if ( n > 10){
+      break
+    }
+    n = n +1
+  }
+
+}
+
+print(n)
+
+// results 3, 4, 5, 6, 7, 8, 9, 10
