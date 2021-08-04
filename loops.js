720a63fcface72b17f7a3fe780d45fd33ecb0e8d
Sean Haile <seanhaile.codes@gmail.com>
2021-08-03 23:27:26 -0600

    new problem plus minus

--- loops.js
+++ loops.js
@@ -37,9 +37,48 @@ var n = 3
 
 function print(n){
   while ( n < 11){
-      console.log()
+      console.log(n)
     n++
   }
 }
 
 print(n)
+
+// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line wit 6 places after the decimal. https://www.hackerrank.com/challenges/one-week-preparation-kit-plus-minus/problem?h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one
+
+
+arr = [1,1,0,-1, -1]
+
+//creater a var for the array
+// iterate the variable for +  - and 0;scottyArray, divide each by total length
+
+// result ? .oooooR (6 places after decimal) - print all ratios
+//no return value from function!!!!!
+
+
+arr = [1,1,0,-1, -1]
+
+
+var arrLength = arr.length
+
+var positiveInt = 0 
+var negativeInt = 0
+var zero = 0
+
+function ratio(arr){
+for(var i = 0; i < arrLength; i++){
+ if (arr[i] < 0){
+   negativeInt++
+ }
+ else if (arr[i]> 0) {
+   positiveInt++
+  //  console.log(positiveInt)
+ }
+ else if (arr[i] == 0)
+  zero++
+ }
+}
+
+console.log(negativeInt)
+console.log(positiveInt)
+console.log(zero)x encodeURIComponent
